94178313013c0f7debf36b59939306d19face5fe
Alessandro Franceschi <al@lab42.it>
2014-05-24 10:24:37 +0200

    Basic, not recursiv tp_lookup function

---
+++ lib/puppet/parser/functions/tp_lookup.rb
@@ -0,0 +1,18 @@
+module Puppet::Parser::Functions
+
+  newfunction(:tp_lookup, :type => :rvalue, :doc => "Looks for tp   data. Usage:
+  $tp_packages=tp_lookup($title,'packages')
+  ") do |args|
+  unless args.length == 2
+    raise Puppet::ParseError, ("tp_lookup(): wrong number of arguments (#{args.length}; must be 2)")
+  end
+
+  app = args[0]
+  res = args[1]
+  key = app + "::" + res
+  tpmod = Puppet::Module.find("tp", compiler.environment.to_s).path
+  yamlfile = tpmod + "/data/" + app + "/default.yaml"
+
+  YAML::load(File.open(yamlfile))[key]
+  end
+end
