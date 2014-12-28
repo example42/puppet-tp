56722c6c9439f41cb04bdd23e6a196da36fbd9ef
Alessandro Franceschi <al@lab42.it>
2014-12-28 17:29:53 +0100

    tp::puppi in all its glory

--- manifests/puppi.pp
+++ manifests/puppi.pp
@@ -5,8 +5,6 @@
 # Manages Puppi integration
 # Usage of this define is optional.
 # When used, it requires Puppi.
-# But sure to include puppi class before using
-# the tp::puppi define.
 # 
 # == Parameters
 #
@@ -15,9 +13,10 @@ define tp::puppi (
   $ensure         = present,
 
   $check_enable   = true ,
+  $check_template = 'tp/puppi/check.erb',
 
   $info_enable    = true ,
-  $info_template  = 'tpdata/puppi/info.erb',
+  $info_template  = 'tp/puppi/info.erb',
   $info_defaults  = true,
 
   $log_enable     = true ,
@@ -31,6 +30,9 @@ define tp::puppi (
   $verbose        = false,
 
   ) {
+
+  # If we use tp::puppi we need puppi
+  include puppi
 
   # Parameters validation
   validate_bool($check_enable)
@@ -46,31 +48,35 @@ define tp::puppi (
   $tp_settings=tp_lookup($title,'settings',$data_module,'merge')
   $settings=merge($tp_settings,$settings_hash)
 
-  # Default options 
+  # Default options and computed variables
   $puppi_defaults = {
-    check_timeout          = '10',
+    check_timeout          => '10',
 
-    check_process_command  = 'check_procs',
-    check_process_critical = '1:',
-    check_process_warning  = undef,
-    check_process_metric   = 'PROCS',
+    check_process_command  => 'check_procs',
+    check_process_critical => '1:',
+    check_process_warning  => '1:',
+    check_process_metric   => 'PROCS',
 
-    check_service_command  = 'service',
+    check_service_command  => 'service',
 
-    check_port_command     = 'check_tcp',
-    check_port_critical    = '10',
-    check_port_warning     = '5',
+    check_port_command     => 'check_tcp',
+    check_port_critical    => '10',
+    check_port_warning     => '5',
+    check_port_host        => '127.0.0.1',
   }
 
   $puppi_options = merge($puppi_defaults, $options_hash)
 
-  $real_check_process_command = ${puppi_options[check_process_command]} ? {
-    'check_procs' => "${puppi_options[check_process_command]} \
+  $real_check_base_dir = pick($puppi_options[check_command_base_dir],$::puppi::checkpluginsdir)
+
+  $real_check_process_command = $puppi_options[check_process_command] ? {
+    'check_procs' => "${real_check_base_dir}/${puppi_options[check_process_command]} \
        -c ${puppi_options[check_process_critical]} \
        -w ${puppi_options[check_process_warning]} \
        -m ${puppi_options[check_process_metric]} \
+       -C ${settings[process_name]} \
       ",
-    default       => ${puppi_options[check_process_command]},
+    default       => "${real_check_base_dir}/${puppi_options[check_process_command]}",
   }
 
   $real_check_service_command = $puppi_options[check_service_command] ? {
@@ -79,46 +85,31 @@ define tp::puppi (
   }
 
   $real_check_port_command = $puppi_options[check_port_command] ? {
-    'check_tcp' => "${puppi_options[check_port_command]} \
-       -H ${puppi_options[check_port_hostname]} \
+    'check_tcp' => "${real_check_base_dir}/${puppi_options[check_port_command]} \
+       -H ${puppi_options[check_port_host]} \
        -c ${puppi_options[check_port_critical]} \
        -w ${puppi_options[check_port_warning]} \
-       -p ${settings[port_name]} \
+       -p ${settings[tcp_port]} \
      ",
-    default     => $puppi_options[check_port_command],
+    default     => "${real_check_base_dir}/${puppi_options[check_port_command]}",
   }
 
-  $real_check_base_dir = pick($puppi_options[check_command_base_dir],$nagios_plugins_dir)
+  $array_package_name=any2array($settings['package_name'])
+  $array_service_name=any2array($settings['service_name'])
+  $array_log_file_path=any2array($settings['log_file_path'])
+  $array_tcp_port=any2array($settings['tcp_port'])
+  $array_info_commands=any2array($settings['info_commands'])
 
   # Puppi checks
   if $check_enable == true {
-
-    if $settings[tcp_port] {
-
-      puppi::check { "${title}_port":
-        description => "Check ${title} TCP port ${settings[tcp_port]}",
-        command     => $real_check_port_command,
-        base_dir    => $real_check_base_dir,
-        hostwide    => true,
-      }
-    }
-
-    if $settings[service_name] {
-      puppi::check { "${title}_service":
-        description => "Check ${title} service",
-        command     => $real_check_service_command,
-        base_dir    => $real_check_base_dir],
-        hostwide    => true,
-      }
-    }
-
-    if $settings[process_name] {
-      puppi::check { "${title}_process":
-        description => "Check ${title} process",
-        command     => $real_check_process_command,
-        base_dir    => $real_check_base_dir],
-        hostwide    => true,
-      }
+    file { "${::puppi::params::checksdir}/${title}":
+      ensure  => $ensure,
+      mode    => '0755',
+      owner   => $::puppi::params::configfile_owner,
+      group   => $::puppi::params::configfile_group,
+      require => Class['puppi'],
+      content => template($check_template),
+      tag     => 'tp_puppi_check',
     }
   }
 
@@ -155,7 +146,7 @@ define tp::puppi (
   # Puppi log
   if $log_enable == true {
     $logs = any2array($settings[log_file_path])
-    file { "${::puppi::params::logdir}/${title}":
+    file { "${::puppi::params::logsdir}/${title}":
       ensure  => $ensure,
       mode    => '0750',
       owner   => $::puppi::params::configfile_owner,
