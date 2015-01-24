a600163d73eb118599c561583a9573640f1db261
Alessandro Franceschi <al@lab42.it>
2015-01-24 21:06:50 +0100

    Some (failed) tests to automate Ruby 1.9 istallation on Centos6

--- vagrant/bin/setup-redhat6.sh
+++ vagrant/bin/setup-redhat6.sh
@@ -7,28 +7,20 @@ case "$1" in
   *) puppetsuffix="-${1}.el6" ;;
 esac
 
-echo "## Installing SCL and Ruby 193"
-package=ruby-1.9.3.p547-1.el6.x86_64.rpm
-[ -f /tmp/$package ] || wget --quiet http://rpms.southbridge.ru/rhel6/ruby-1.9.3/x86_64/$package -O /tmp/$package
-rpm -qi ruby193 >/dev/null 2>&1
-if [ "x$?" == "x1" ] ; then
-  yum install -y /tmp/$package
-fi
-
-
-echo "## Installing latest Puppet version and dependencies"
+echo "## Installing Puppet "
 
 rpm -qi epel-release >/dev/null 
 if [ "x$?" == "x1" ] ; then
-  rpm -ivh http://ftp.colocall.net/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm >/dev/null # 2>&1
+  rpm -ivh http://mirror.oss.maxcdn.com/epel/6/i386/epel-release-6-8.noarch.rpm >/dev/null # 2>&1
 fi
 
-if [ "x$1" != "xoriginal" ] ; then
-  rpm -qa | grep puppetlabs-release >/dev/null # 2>&1
-  if [ "x$?" == "x1" ] ; then
-    rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-11.noarch.rpm >/dev/null # 2>&1
-  fi
-fi
+# Puppetlabs' repo conflicts with SCL Ruby 193
+#if [ "x$1" != "xoriginal" ] ; then
+#  rpm -qa | grep puppetlabs-release >/dev/null # 2>&1
+#  if [ "x$?" == "x1" ] ; then
+#    rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm >/dev/null # 2>&1
+#  fi
+#fi
 
 rpm -qi puppet$puppetsuffix >/dev/null 2>&1
 if [ "x$?" == "x1" ] ; then
