45823c01544da3baffdc7166dffeb00df8d58bb4
Alessandro Franceschi <al@lab42.it>
2015-02-09 11:25:11 +0100

    Added check_files.sh script

--- bin/verify_yaml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-basedir=`dirname $0`
-datadir="$basedir/../data"
-
-for i in `find $datadir -type f -name *.yaml`; do
-    echo $i
-    ruby -e "require 'yaml'; YAML.parse(File.open('$i'))"
-done
