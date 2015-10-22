da1ba16c5ece1e8bbf603e62c9977fc8a74c7807
Alessandro Franceschi <al@example42.com>
2015-10-22 18:10:22 +0200

    Excluding dependency modules from coveralls stats

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'simplecov'
 require 'coveralls'
 if ENV['PARSER'] == 'future'
   RSpec.configure do |c|
     c.parser = 'future'
   end
 end
-Coveralls.wear!
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'spec/fixtures/modules/'
+end
