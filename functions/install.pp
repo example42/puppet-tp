b9823d5d52b0fc6265b175b59929cd9543210d1a
Alessandro Franceschi <al@example42.com>
2016-10-18 16:04:10 -0700

    Small docs for tp::install function

--- functions/install.pp
+++ functions/install.pp
@@ -1,3 +1,8 @@
+# This function runs the tp::install define trying
+# to avoid duplicated resources issues the tp::install
+# may be declared multiple times (with same parameters)
+# It is equivalent to the tp_install function, written in Ruby
+#
 function tp::install (
   String $app,
   Hash   $params = { },
