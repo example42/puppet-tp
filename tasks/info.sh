e5f590e5fde83ee32977e7ac774db669a143c97f
Alessandro Franceschi <al@example42.com>
2022-11-08 12:17:49 +0100

    Added tp::version task. Fixes and preparing for 3.5.0

--- tasks/info.sh
+++ tasks/info.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 PATH=$PATH:/usr/local/bin
+
 [[ -n "${PT_app}" ]] && app="${PT_app}"
-[[ -n "${PT_block}" ]] && block="${PT_block}"
-tp info "${app}" "${block}"
+if [[ -n "${PT_block}" ]]; then
+  tp info "${app}" "${PT_block}"  2>&1
+else
+  tp info "${app}" 2>&1
+fi
